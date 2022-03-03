43f621a5bb92af75e4ee353bf029d82c34cca389
clinton wambugu <clintonwambugu@gmail.com>
2022-03-03 21:48:12 +0300

    Minor bug fix

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 let tip = 0.00;
 let total = 0.00;
-let bill = document.getElementsByClassName("bill-el")[0].value;
-let noOfPeople = document.getElementsByClassName("people-el")[0].value;
+let bill = document.getElementById("bill-el").value;
+let noOfPeople = document.getElementById("people-el").value;
 let tipResultEl = document.getElementById("tip-result-el");
 let totalResultEl = document.getElementById("total-result-el");
 
 
 function getTip(percentage) {
-  let tip = bill * percentage / 100;
+  let tip = parseInt(bill) * percentage / 100;
   let tipPerPerson = tip / noOfPeople;
   let totalPerPerson = parseInt(bill) + tipPerPerson;
 
