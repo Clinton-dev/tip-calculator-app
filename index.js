7671f2cb5eed49ced6c72dfc44b57b8d80794b15
clinton wambugu <clintonwambugu@gmail.com>
2022-03-04 14:54:59 +0300

    Fix NaN bug

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-let tip = 0.00;
-let total = 0.00;
-let bill = document.getElementById("bill-el").value;
-let noOfPeople = document.getElementById("people-el").value;
 let tipResultEl = document.getElementById("tip-result-el");
 let totalResultEl = document.getElementById("total-result-el");
 
 
 function getTip(percentage) {
+  let bill = document.getElementById("bill-el").value;
+  let noOfPeople = document.getElementById("people-el").value;
   let tip = parseInt(bill) * percentage / 100;
   let tipPerPerson = tip / noOfPeople;
   let totalPerPerson = parseInt(bill) + tipPerPerson;
